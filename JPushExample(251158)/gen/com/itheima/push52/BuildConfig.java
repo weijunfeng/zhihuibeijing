1852331acf2479263ec00a1f34d9ad73ea29d132
weijunfeng <891130789@qq.com>
2016-05-04 17:10:59 +0800

    删除无用文件

--- JPushExample(251158)/gen/com/itheima/push52/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.itheima.push52;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- 扫描二维码/gen/com/libs/zxing/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.libs.zxing;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
